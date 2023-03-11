fcff5eaa46d005dd22ed23cfbe375494a5c2540e
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-03-11 15:58:55 -0600

    update

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,8 +9,13 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }],
-      include: [{ model: ProductTag}],
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
     });
     res.status(200).json(productData)
 } catch (err){
@@ -19,9 +24,27 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findByPk(req.params.id,{
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+    if (!productData){
+      res.status(404).json({ message: 'No product found'});
+      return;
+    }
+    res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 // create new product
