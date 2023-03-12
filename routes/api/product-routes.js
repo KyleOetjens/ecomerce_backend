57194391639d200b6608adbc41ab58aeb00a4fc5
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-03-12 14:59:01 -0500

    product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -133,7 +133,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({message: 'No product found'});
       return;
     }
-    res.status(200).json(productData);
+    res.status(200).json({message: 'Product deleted'});
   } catch (err) {
     res.status(500).json(err);
   }
